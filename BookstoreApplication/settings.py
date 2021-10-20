e3e5a3c02624a1a94a3447edab869e527a3df411
Kolaseq <michal.kolasniewski@apz.pl>
2021-10-20 08:47:12 +0200

    update

--- BookstoreApplication/settings.py
+++ BookstoreApplication/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+DEBUG_COLLECTSTATIC=1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
